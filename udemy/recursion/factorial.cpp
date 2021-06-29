b4e5648b75d9d9ae100623b40a649a6153edb451
khushab <khushabalam7@gmail.com>
2021-06-29 18:58:06 +0530

    alternative added

--- udemy/recursion/factorial.cpp
+++ udemy/recursion/factorial.cpp
@@ -13,7 +13,22 @@ int fact(int num)
     }
 }
 
+int factTwo(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
 int main()
 {
-    cout << fact(5);
+    cout << fact(5) << endl;
+    cout << factTwo(5);
 }
