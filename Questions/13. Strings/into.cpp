ac57f564f142dedf2e28733b90d2d5a2276f0d5f
khushab <khushabalam7@gmail.com>
2021-06-30 09:37:08 +0530

    intro completed

--- Questions/13. Strings/into.cpp
+++ Questions/13. Strings/into.cpp
@@ -43,5 +43,34 @@ int main()
     s4.erase(1, 2);
     cout << s4 << endl;
 
+    //find a substring in a string
+    string s5 = "Khshab";
+    cout << s5.find("sha") << endl;
+
+    //insert in string
+    s5.insert(2, "u");
+    cout << s5 << endl;
+
+    //find size
+    cout << s5.size() << endl;
+    cout << s5.length() << endl;
+
+    //substring
+    string s = s5.substr(1, 4);
+    cout << s << endl;
+
+    //strnig to integer
+    string numString = "123";
+    int x = stoi(numString);
+    cout << x + 2 << endl;
+
+    //integer to string
+    cout << to_string(x) + "2" << endl;
+
+    //sort string
+    string unsorted = "asrerfsfnoqkpwkeqas";
+    sort(unsorted.begin(), unsorted.end());
+    cout << unsorted << endl;
+
     return 0;
 }
