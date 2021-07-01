b691383d94be59f0fccd3f57695795a332127edd
khushab <khushabalam7@gmail.com>
2021-07-01 14:21:53 +0530

    uppercase

--- Questions/13. Strings/1. Challenges/upperCase.cpp
+++ Questions/13. Strings/1. Challenges/upperCase.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main()
 {
     string str = "khushab";
-    cout << 'a';
-    cout << 'z';
-    cout << 'A';
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+    cout << str;
 }
