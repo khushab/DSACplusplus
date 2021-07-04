6b6e1d06b2b7f08fff2eecf26ba8b8819a1117f6
khushab <khushabalam7@gmail.com>
2021-07-04 18:45:32 +0530

    tower of hanoi

---
+++ Questions/16. Recursion/7 Best problems on Recursion/towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void toh(int n, int a, int b, int c)
+{
+    if (n > 0)
+    {
+        toh(n - 1, a, c, b);
+        cout << "Move " << a << " to " << c << endl;
+        toh(n - 1, b, a, c);
+    }
+}
+int main()
+{
+    toh(2, 1, 2, 3);
+}
